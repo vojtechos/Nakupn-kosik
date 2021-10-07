aa537ed0d07ab94fa20e867c085c71c96cfa8b61
Vojtěch Vít <90755402+vojtechos@users.noreply.github.com>
2021-10-07 14:13:57 +0200

    Update NakupniKosik.java

--- src/com/src/NakupniKosik.java
+++ src/com/src/NakupniKosik.java
@@ -4,20 +4,20 @@ public class NakupniKosik {
     public static void main(String[] args){
         String zbozi[] = {"Rajče", "Jablko", "Cibule", "Brambora", "Mléko", "Chleba", "Lovečák"};
         int ceny[] = {10, 8, 5, 7, 12, 25, 30};
-        int finalcena = 0;
+        int finalniCena = 0;
 
         if(args.length == 0){
             System.out.println("Seznam prduktů: ");
-            for (int i = 0; i < veci.length; i++){
-                System.out.println(i + " - " + veci[i] + " - " + ceny[i] + "Kč,-");
+            for (int i = 0; i < zobzi.length; i++){
+                System.out.println(i + " - " + zbozi[i] + " - " + ceny[i] + "Kč,-");
             }
         }
 
         else{
             for (int i = 0; i < args.length; i++){
-                if(Integer.parseInt(args[i]) <= veci.length-1){
-                    System.out.println(args[i] + " - " + veci[Integer.parseInt(args[i])] + " - " + ceny[Integer.parseInt(args[i])] + "Kc.");
-                    finalcena += ceny[Integer.parseInt(args[i])];
+                if(Integer.parseInt(args[i]) <= zbozi.length-1){
+                    System.out.println(args[i] + " - " + zbozi[Integer.parseInt(args[i])] + " - " + ceny[Integer.parseInt(args[i])] + "Kc.");
+                    finalniCena += ceny[Integer.parseInt(args[i])];
                 }
 
                 else{
@@ -25,7 +25,7 @@ public class NakupniKosik {
                 }
             }
 
-            System.out.println("Finální cena " + finalcena + "Kč,-");
+            System.out.println("Finální cena " + finalniCena + "Kč,-");
         
         }
     }
